0ca30e3a7f3951aba1bffa912d91c6afc3d03915
sacastillo6 <sacastillo6@gmail.com>
2017-01-19 21:41:22 -0600

    more changes yo

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,11 @@
-$('.toggle-nav').on('click',function(){
-	$('.MainNav ul').addClass('active');
-	});
+console.log('Js is live!');
+
+$('.hamburger-icon').on('click', function(){
+	console.log('Button works.');
+
+    $('.menu-wrap').toggleClass('menu-show');
+    $('.menu2').toggleClass('menu2-show');
+    console.log('sidebar works');
+
+    $(this).toggleClass('active');
+});
